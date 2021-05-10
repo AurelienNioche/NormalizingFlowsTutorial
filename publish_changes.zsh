35dc874d5fc88e34f08e3158b702ce86cee52ca4
AurelienNioche <nioche.aurelien@gmail.com>
2021-05-10 16:42:41 +0300

    update

--- publish_changes.zsh
+++ publish_changes.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh 
 echo "converting notebook into html..."
-jupyter-nbconvert --to html normalizing-flows-tutorial.ipynb --output docs/normalizing-flows-tutorial.html
+jupyter-nbconvert --to html normalizing-flows-tutorial.ipynb --output docs/normalizing-flows-tutorial.html --template toc2
 echo "copying images..."
 cp -r img docs
 echo "done!"
